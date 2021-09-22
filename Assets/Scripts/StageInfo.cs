49c59268c873c74ad2a9d619bae465689602eab2
hoyeoncho <hoyeon1107@gmail.com>
2021-09-22 20:38:10 +0900

    joho
    test commit

--- Assets/Scripts/StageInfo.cs
+++ Assets/Scripts/StageInfo.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class StageInfo : MonoBehaviour
 {
+    public int iStageIndex = 0;
     public int iStageScore = 0;
     public int iStageStars = 0;
+    public int iCurrentPassCount = 0;
 
     void Start()
     {
